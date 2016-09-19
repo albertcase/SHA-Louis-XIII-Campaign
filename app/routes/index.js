17a2b57bd31d3a22e2a96280c1424a7262e50ec6
Albert Shen <albertshen1206@gmail.com>
2016-09-19 17:23:27 +0800

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,29 +6,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello Albert' });
 });
 
-router.get('/gm', function(req, res, next) {
+router.get('/message/:id', function(req, res, next) {
+  res.render('test', { title: req.params.id });
+});
+
+router.post('/ajax/api/message', function(req, res, next) {
+	var name = req.body.name;
+	var message = req.body.message;
 	var fs = require('fs'),
   		gm = require('gm');
-
 	gm('../images/pic.png')
 	.stroke("#ffffff")
 	.font("Helvetica.ttf", 25)
 	.drawText(120, 20, "GMagick!")
 	.write("../images/pic2.png", function (err) {
 	  if (!err) {
-	  	console.log('done')
+	  	var mysql = require('mysql');
+	  	//....
+		res.setHeader('Content-Type', 'application/json');
+    	res.send(JSON.stringify({ status: 1, image_src: 'http://xxxxxx' }));
 	  } else {
 	  	console.log(err)
 	  };
-	});
-
-	res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ status: 1, url: 'xxxxx' }));
+	});	
 });
 
-router.get('/mysql', function(req, res, next) {
-	var mysql = require('mysql');
+router.get('/qrcode-demo', function(req, res, next) {
+	var qr_image = require('qr-image');
 	//code
+});
+
+router.get('/uuid-demo', function(req, res, next) {
+	var uuid = require('node-uuid');  
+	res.send(JSON.stringify({uuid: uuid.v4() }));
+
 });
 
 module.exports = router;
