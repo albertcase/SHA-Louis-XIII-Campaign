ed3a19402111346b32978c7e2510120c484c2876
Demon Zhang <demon.zhang@samesamechina.com>
2016-11-21 17:00:16 +0800

    no message

--- app/routes/index.js
+++ app/routes/index.js
@@ -97,13 +97,13 @@ router.post('/ajax/api/message', function(req, res, next) {
 			var start = (1133 -(name.length-1)*32)/2;
 		} else {
 			var font = 'Cresci_LP';
-			var start = (1133-(name.length-1)*20)/2;
+			var start = (1133-(name.length-1)*25)/2;
 		}
 		console.log(font)
 	    gm('../images/tu_empty2.jpg')
 		.stroke("#ffffff")
 		.font("../images/"+ font +".ttf", 60)
-		.drawText(start, 560, name)
+		.drawText(start, 460, name)
 		//.draw(['image over 190,775 140,140 "http://louisxiii-cognac.samesamechina.com/qrcode-demo/'+ id +'"'])
 		.resize(600, 600, "!")
 		.autoOrient()
