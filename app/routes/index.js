bb2293a49eb78d6c161d3c3c6aa3bceb8f7e2b46
Demon Zhang <demon.zhang@samesamechina.com>
2016-11-24 12:09:36 +0800

    no message

--- app/routes/index.js
+++ app/routes/index.js
@@ -100,7 +100,8 @@ router.post('/ajax/api/message', function(req, res, next) {
 			var m=(name.toLowerCase().split('w')).length-1;
 			var o=(name.toLowerCase().split('m')).length-1;
 			var n=(name.toLowerCase().split('i')).length-1;
-			var start = (1133-(name.length-n-m-o)*50 - n*25 - m*80 - o*80)/2;
+			var j=(name.toLowerCase().split('j')).length-1;
+			var start = (1133-(name.length-n-m-o-j)*50 - n*25 - m*80 - o*80 - j*25)/2;
 		}
 		//console.log(font)
 
